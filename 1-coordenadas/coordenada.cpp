15805df63a6cf1c2c0ac4b8d9c68f842c4a79a92
Andrés Fernández <andresf91@gmail.com>
2025-03-13 14:58:37 -0300

    Comienzo cabezales de coordenada

---
+++ 1-coordenadas/coordenada.cpp
@@ -0,0 +1,2 @@
+#include "coordenada.h"
+
---
+++ 1-coordenadas/coordenada.h
@@ -0,0 +1,16 @@
+#ifndef COORDENADA_H
+#define COORDENADA_H
+
+class Coordenada {
+    private:
+        int x;
+        int y;
+    
+    public:
+        int getX();
+        int getY();
+        int setX(int x);
+        int setY(int y);
+};
+
+#endif
